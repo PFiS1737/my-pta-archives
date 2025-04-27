4903a51e8bbbb9a74d097c4f587bce9daf330715
PFiS1737 <pfis1737@gmail.com>
2025-04-28 00:06:29 +0800

    feat: 第三次作业

---
+++ archives/11_3_爬楼梯(递归).cpp
@@ -0,0 +1,30 @@
+// 楼梯有 n 级台阶，每一步可以跨越 1 ~ 3 级台阶。请问一共有多少种上法。
+// 说明：参数 step 为台阶数。函数值为爬楼梯的方法总数。
+
+// Sample Input
+// 15
+
+// Sample Output
+// 5768
+
+// Append Code Start
+#include <stdio.h>
+
+double Climb(int step);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%g\n", Climb(n));
+    return 0;
+}
+// Append Code End
+
+double Climb(int step) {
+  return step < 0
+    ? 0
+    : step == 0
+      ? 1
+      : Climb(step - 1) + Climb(step - 2) + Climb(step - 3);
+}
